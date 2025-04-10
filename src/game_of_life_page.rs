6aba9c596d61586236d8bcd33e31c6a4b0704b3a
Julian Górski <julobrat@gmail.com>
2025-04-10 19:00:50 +0200

    feat: add ability to click on cells and change them

--- src/game_of_life_page.rs
+++ src/game_of_life_page.rs
@@ -60,21 +60,27 @@ fn GameOfLife() -> impl IntoView {
             <input type="submit" value="Set" />
         </form>
 
-        <Board board=board width=width/>
+        <Board board=board set_board=set_board width=width/>
     }
 }
 
 #[component]
-fn Board(board: ReadSignal<Vec<Vec<bool>>>, width: ReadSignal<usize>) -> impl IntoView {
+fn Board(
+    board: ReadSignal<Vec<Vec<bool>>>,
+    set_board: WriteSignal<Vec<Vec<bool>>>,
+    width: ReadSignal<usize>,
+) -> impl IntoView {
     let cells = move || {
         board
             .get()
             .iter()
-            .map(|row| {
+            .enumerate()
+            .map(|(y, row)| {
                 row.iter()
-                    .map(move |&is_alive| {
+                    .enumerate()
+                    .map(move |(x, &is_alive)| {
                         view! {
-                            <Cell is_alive=is_alive />
+                            <Cell set_board=set_board x=x y=y is_alive=is_alive />
                         }
                     })
                     .collect::<Vec<_>>()
@@ -90,11 +96,26 @@ fn Board(board: ReadSignal<Vec<Vec<bool>>>, width: ReadSignal<usize>) -> impl In
 }
 
 #[component]
-fn Cell(is_alive: bool) -> impl IntoView {
+fn Cell(
+    set_board: WriteSignal<Vec<Vec<bool>>>,
+    x: usize,
+    y: usize,
+    is_alive: bool,
+) -> impl IntoView {
+    let on_click = move |_| {
+        set_board.update(|board| {
+            if let Some(row_vec) = board.get_mut(y) {
+                if let Some(cell) = row_vec.get_mut(x) {
+                    *cell = !*cell;
+                }
+            }
+        });
+    };
+
     let color = move || if is_alive { "black" } else { "gray" };
 
     view! {
-        <div class="cell" style:background-color=color></div>
+        <div class="cell" style:background-color=color on:click=on_click></div>
     }
 }
 
